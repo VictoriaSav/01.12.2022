a3df993e6850457adf9cbba6f363e4d210d5f7b5
Victoriia Savytska <104302376+VictoriaSav@users.noreply.github.com>
2022-12-01 23:01:30 +0100

    start

---
+++ javascript-beginner/app.js
@@ -0,0 +1,7 @@
+// ПЕРЕМЕННЫЕ //
+//var, varieble - переменная, переменный
+const firstName = "Victa";
+
+const lastName = "Minin"; //string
+let age = 28; //number
+const isProgrammer = true; //boolean
