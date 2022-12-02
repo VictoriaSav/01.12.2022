c043c9ea08d339bfd05c7d8d1a9b218f58080f5c
Victoriia Savytska <104302376+VictoriaSav@users.noreply.github.com>
2022-12-02 23:18:42 +0100

    Update app.js

--- javascript-beginner/app.js
+++ javascript-beginner/app.js
@@ -1,9 +1,50 @@
 // ПЕРЕМЕННЫЕ //
 //var, varieble - переменная, переменный
-const firstName = "Victa";
+// const firstName = "Victa";
+// // let age = 28; //number
+// const isProgrammer = true; //boolean
 
-const lastName = "Minin"; //string
-let age = 28; //number
-const isProgrammer = true; //boolean
+// const _private = "private";
+// const $ = "some value";
 
-const _private = "private";
+// const if = 'mkef' error
+
+// const withNum5 = "5";
+// const 5wfeNum = '5 ' error
+
+// / МУЛЬТИРОВАНИЕ //
+// console.log("Имя человека: " + firstName + ", а возраст человека: " + age);
+// alert("Имя человека: " + firstName + ", а возраст человека: " + age);
+
+// const lastName = prompt("Введите фамилию");
+// alert(firstName + "" + lastName);
+
+// ОПЕРАТОРЫ //
+// Базовые операторы для чисел
+// let currentYear = 2022;
+// const birthYear = 1994;
+// // console.log(age);
+// // const age = currentYear + birthYear;
+
+// const a = 10;
+// const b = 5;
+
+// let c = 32;
+// c = c + a;
+// c += a;
+// c = c - a;
+// c -= a;
+// c = c / a;
+// c /= a;
+// c = c * a;
+// c *= a;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(++currentYear);
+// console.log(currentYear);
+// console.log(c);
+
+// ТИПЫ ДАННЫХ//
