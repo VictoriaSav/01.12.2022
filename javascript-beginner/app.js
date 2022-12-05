aa41f6cdd36c570a8f6c50ab7b946962274e4535
Victoriia Savytska <104302376+VictoriaSav@users.noreply.github.com>
2022-12-05 22:54:03 +0100

    final

--- javascript-beginner/app.js
+++ javascript-beginner/app.js
@@ -1,9 +1,68 @@
-const person = {
-    name: 'Victa',
-    age: 28,
-    isProgrammer: true,
-    languages: ['ru', 'en', 'de'],
-    greet: function () {
-        
-    }
+const heading = document.getElementById("hello");
+// const heading2 = document.getElementsByTagName('h2')[0]
+// const heading2 = document.getElementsByClassName('h2-class')[0]
+// const heading2 = document.querySelector('.h2-class')
+// const heading2 = document.querySelector('#sub-hello') // Всегда 1 элемент
+const heading2 = document.querySelector("h2");
+console.log(heading2);
+
+// const heading3 = heading2.nextElementSibling
+const h2List = document.querySelectorAll("h2");
+const heading3 = h2List[h2List.length - 1];
+
+const link = heading3.querySelector("a");
+
+link.addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log("Click on link", event.target.getAttribute("href"));
+  const url = event.target.getAttribute("href");
+
+  window.location = url;
+});
+
+setTimeout(() => {
+  addStylesTo(heading, "JavaScript");
+}, 1500);
+
+setTimeout(() => {
+  addStylesTo(heading3, "Практикуйся", "blue");
+}, 3000);
+
+setTimeout(() => {
+  addStylesTo(heading2, "И все получится!", "yellow", "2rem");
+}, 4500);
+
+function addStylesTo(node, text, color = "red", fontSize) {
+  heading.textContent = "Changed from JavaScript!";
+  heading.style.color = "red";
+  heading.style.textAlign = "center";
+  heading.style.backgroundColor = "black";
+  heading.style.padding = "2rem";
+  heading.style.display = "block";
+  heading.style.width = "100%";
+
+  // falsy: '', undefined, null, 0, false
+  if (fontSize) {
+    node.style.fontSize = fontSize;
+  }
 }
+
+heading.onclick = () => {
+  if (heading.style.color === "red") {
+    heading.style.color = "#000";
+    heading.style.backgroundColor = "#fff";
+  } else {
+    heading.style.color = "red";
+    heading.style.backgroundColor = "#000";
+  }
+};
+
+heading2.addEventListener("dblclick", () => {
+  if (heading2.style.color === "yellow") {
+    heading2.style.color = "#000";
+    heading2.style.backgroundColor = "#fff";
+  } else {
+    heading2.style.color = "yellow";
+    heading2.style.backgroundColor = "#000";
+  }
+});
